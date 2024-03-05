7acf8d7702f383d8dc421b956ca5e4f2b3cde839
jaepang <jake_king@kakao.com>
2024-03-05 21:55:40 +0900

    add fitting setting ui rendering condition

--- src/components/Layout/SimpleConfig/index.tsx
+++ src/components/Layout/SimpleConfig/index.tsx
@@ -5,7 +5,8 @@ export default function SimpleConfig() {
   const { info, setInfo } = useContext(InfoContext)
   const { colors: colorsString, fittingColor, fittingSize, size } = info.simple
   const colors = colorsString?.split(',')
-  const sizes = size?.map((size) => size.name.includes('(') ? size.name.slice(0, size.name.indexOf('(')) : size.name)
+  const sizes = size?.map((size) => (size.name.includes('(') ? size.name.slice(0, size.name.indexOf('(')) : size.name))
+  const showFitting = colors?.length > 0 && sizes?.length > 0
 
   function onFittingChange(e, target) {
     const newSet = info.simple[target]
@@ -60,41 +61,43 @@ export default function SimpleConfig() {
           onChange={(e) => onChange(e, 'imageLength')}
         />
       </div>
-      <div>
-        <h2 style={{ marginBottom: 0 }}>피팅 정보 입력</h2>
-        <div style={{ display: 'flex', alignItems: 'flex-start', gap: '12px' }}>
-          <div>
-            <h3>컬러</h3>
-            {colors?.map((name) => (
-              <div key={name}>
-                <input
-                  id={name}
-                  type="checkbox"
-                  checked={fittingColor.has(name)}
-                  value={name}
-                  onChange={(e) => onFittingChange(e, 'fittingColor')}
-                />
-                <label htmlFor={name}>{name}</label>
-              </div>
-            ))}
-          </div>
-          <div>
-            <h3>사이즈</h3>
-            {sizes?.map((size) => (
-              <div key={size}>
-                <input
-                  id={size}
-                  type="checkbox"
-                  checked={fittingSize.has(size)}
-                  value={size}
-                  onChange={(e) => onFittingChange(e, 'fittingSize')}
-                />
-                <label htmlFor={size}>{size}</label>
-              </div>
-            ))}
+      {showFitting && (
+        <div>
+          <h2 style={{ marginBottom: 0 }}>피팅 정보 입력</h2>
+          <div style={{ display: 'flex', alignItems: 'flex-start', gap: '12px' }}>
+            <div>
+              <h3>컬러</h3>
+              {colors?.map((name) => (
+                <div key={name}>
+                  <input
+                    id={name}
+                    type="checkbox"
+                    checked={fittingColor.has(name)}
+                    value={name}
+                    onChange={(e) => onFittingChange(e, 'fittingColor')}
+                  />
+                  <label htmlFor={name}>{name}</label>
+                </div>
+              ))}
+            </div>
+            <div>
+              <h3>사이즈</h3>
+              {sizes?.map((size) => (
+                <div key={size}>
+                  <input
+                    id={size}
+                    type="checkbox"
+                    checked={fittingSize.has(size)}
+                    value={size}
+                    onChange={(e) => onFittingChange(e, 'fittingSize')}
+                  />
+                  <label htmlFor={size}>{size}</label>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
